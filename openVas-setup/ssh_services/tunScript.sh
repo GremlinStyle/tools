7e80423c50ede73659bb6bc2b43c6cbc45c88c7d
GremlinStyle <152166424+GremlinStyle@users.noreply.github.com>
2024-04-10 14:30:55 +0200

    Update tunScript.sh

--- openVas-setup/ssh_services/tunScript.sh
+++ openVas-setup/ssh_services/tunScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Openvas Con
-ssh -N -o StrictHostKeyChecking=no -i /home/kali/adamMADEtunnel/ubuntu-test.pem -R 8081:localhost:9392 ubuntu@16.171.200.130
+ssh -N -o StrictHostKeyChecking=no -i $keypath$ -R 8081:localhost:9392 $sshconection$

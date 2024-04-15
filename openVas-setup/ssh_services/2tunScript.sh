a9bc1f41911ad54b4f321c13aed0b510b1673e7a
GremlinStyle <152166424+GremlinStyle@users.noreply.github.com>
2024-04-15 10:41:16 +0200

    Update 2tunScript.sh

--- openVas-setup/ssh_services/2tunScript.sh
+++ openVas-setup/ssh_services/2tunScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SSH Con
-#ssh -N -o StrictHostKeyChecking=no -i $keypath$ -R 8080:localhost:22 $sshconection$
+#ssh -N -o StrictHostKeyChecking=no -i $keypath$ -R 8080:localhost:22 $sshco$
 ssh -N -o StrictHostKeyChecking=no -R $PORT$:localhost:22 $sshconection$

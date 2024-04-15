f0c4e8cf292055a60c398eec5d6682187635acd8
GremlinStyle <152166424+GremlinStyle@users.noreply.github.com>
2024-04-15 10:36:15 +0200

    Update tunScript.sh

--- openVas-setup/ssh_services/tunScript.sh
+++ openVas-setup/ssh_services/tunScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Openvas Con
 #ssh -N -o StrictHostKeyChecking=no -i $keypath$ -R 8081:localhost:9392 $sshconection$
-ssh -N -o StrictHostKeyChecking=no -R $port2$:localhost:9392 $sshconection$
+ssh -N -o StrictHostKeyChecking=no -R $PORT$:localhost:9392 $sshconection$

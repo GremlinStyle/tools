eb0a811400eb885c27fc6505ebd97ef876bbfdf3
GremlinStyle <152166424+GremlinStyle@users.noreply.github.com>
2024-04-22 15:22:30 +0200

    Check envar
    If the install script is cancled during it's deed there are maybe already var saved goes through each and asks if they are correct

--- openVas-setup/setup/2.0/master2.0-setup.sh
+++ openVas-setup/setup/2.0/master2.0-setup.sh
@@ -8,7 +8,28 @@ SCRIPTPATH=/root/scripts
 
 #START OF SET-VARIBLE 
 
+text=("\n\e[96m\e[1m[*]\e[0m Please enter \e[33mthe username\e[0m of your server: " "\n\e[96m\e[1m[*]\e[0m Please enter \e[33mthe hostname\e[0m of your server: " "\n\e[96m\e[1m[*]\e[0m Please leave the \e[31mpassword field NOT empty\e[0m at the ssh-key generation but if you want everything else: " "\n\e[96m\e[1m[*]\e[0m Please enter the ssh key password again for me" "\n\e[96m\e[1m[*]\e[0m Now we will use ssh-copy-id to put the freshly generated key onto the server for which will allow us to connect to the server without any need for passwords or keys\n\n\tPlease Enter the \e[31mcomplete path\e[0m to the identity file of the \e[33mserver\e[0m:" "\n\e[96m\e[1m[*]\e[0m Please enter the \e[31mfirst port\e[0m of the server which will be used to tunnel \e[32mssh\e[0m to the server for remote access indepedant of it's network properties:" "\n\e[96m\e[1m[*]\e[0m Please enter the \e[31msecond port\e[0m of the server which will be used to tunnel the \e[32mWebpage\e[0m to the server for accessing the webpage on the main server:" "\n\e[96m\e[1m[*]\e[0m Please enter a name for the \e[31mnew Openvas user\e[0m: " "\n\e[96m\e[1m[*]\e[0m Please enter a \e[31mpassword\e[0m for the new Openvas user: " "\n\e[96m\e[1m[*]\e[0m  Please enter the email used \e[33mto send reports\e[0m: " "\n\e[96m\e[1m[*]\e[0m Please type the \e[31mAppkey\e[0m of the email you use to send reports: " "\n\e[96m\e[1m[*]\e[0m  Please type the email used \e[33mto receive reports\e[0m: ")
 
+if [ -e /root/scripts/envar.conf ]; then 
+echo -e "A config File was found we will use this one to speed things up\nIf the Variables a right please hit enter without typing anything"; 
+
+len=$(cat /root/scripts/envar.conf | wc -l)
+for ((i=1; i<=len; i++)); do
+    line=$(sed -n "${i}p" /root/scripts/envar.conf)
+    IFS="="
+    read -ra newarr <<< "$line"
+    echo -e "If the answer is correct please hit enter to proceed otherwise enter the correct answer:${text[$i-1]}\nAnswer found in old config file: ${newarr[1]}\n"
+    read ans
+    if [ ${#ans} -gt 1 ]; then
+    declare ${newarr[0]}=$ans
+    ne="${newarr[0]}=$ans"
+    sed -i "${i}s/.*/${ne}/" /root/scripts/envar.conf
+    echo -e "answer is changed\n"
+    else
+    echo "answer is unchanged\n"
+    fi
+done
+fi
 
 #SSH CONFIG
 
@@ -21,9 +42,11 @@ if [ $check == y ]; then echo "Ingore: We will proceed"; else echo "Rude: then p
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter \e[33mthe username\e[0m of your server: "
 read SCONU
+echo "SCONU=$SCONU" >> /root/scripts/envar.conf
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter \e[33mthe hostname\e[0m of your server: "
 read SCONI
+echo "SCONI=$SCONI" >> /root/scripts/envar.conf
 
 SSHCON="$SCONU@$SCONI"
 
@@ -31,11 +54,9 @@ echo -e "\n\e[96m\e[1m[*]\e[0m Please leave the \e[31mpassword field NOT empty\e
 
 ssh-keygen -t rsa
 
-echo -e \n\e[96m\e[1m[*]\e[0m Please enter the password again for me too save it for the secure usage of ssh"
+echo -e "\n\e[96m\e[1m[*]\e[0m Please enter the ssh key password again for me"
 read -s SSHPASSWD
-
-echo -e "\n\e[96m\e[1m[*]\e[0m We will resume with the setup in 2 seconds"
-sleep 2
+echo "SSHPASSWD=$SSHPASSWD" >> /root/scripts/envar.conf
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Now we will use ssh-copy-id to put the freshly generated key onto the server for which will allow us to connect to the server without any need for passwords or keys\n\n\tPlease Enter the \e[31mcomplete path\e[0m to the identity file of the \e[33mserver\e[0m:"
 read pa
@@ -64,25 +85,32 @@ ssh $SSHCON cat .ssh/id_rsa.pub | tee -a $HOME/.ssh/authorized_keys
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter the \e[31mfirst port\e[0m of the server which will be used to tunnel \e[32mssh\e[0m to the server for remote access indepedant of it's network properties:"
 read PORT1
+echo "PORT1=$PORT1" >> /root/scripts/envar.conf
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter the \e[31msecond port\e[0m of the server which will be used to tunnel the \e[32mWebpage\e[0m to the server for accessing the webpage on the main server:"
 read PORT2
+echo "PORT2=$PORT2" >> /root/scripts/envar.conf
 
 #OPENVAS / GVM CONFIG
 
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter a name for the \e[31mnew Openvas user\e[0m: "
 read GVMUSER
+echo "GVMUSER=$GVMUSER" >> /root/scripts/envar.conf
 echo -e "\n\e[96m\e[1m[*]\e[0m Please enter a \e[31mpassword\e[0m for the new Openvas user: "
 read -s GVMPASWD
+echo "GVMPASWD=$GVMPASWD" >> /root/scripts/envar.conf
 
 #EMAIL CONFIG
 
 echo -e "\n\e[96m\e[1m[*]\e[0m  Please enter the email used \e[33mto send reports\e[0m: "
 read FROMAIL
+echo "FROMAIL=$FROMAIL" >> /root/scripts/envar.conf
 echo -e "\n\e[96m\e[1m[*]\e[0m Please type the \e[31mAppkey\e[0m of the email you use to send reports: "
 read APPKEY
+echo "FROMAIL=$FROMAIL" >> /root/scripts/envar.conf
 echo -e "\n\e[96m\e[1m[*]\e[0m  Please type the email used \e[33mto receive reports\e[0m: "
 read TOMAIL
+echo "FROMAIL=$FROMAIL" >> /root/scripts/envar.conf
 
 
 mkdir -p /root/scripts
@@ -96,20 +124,6 @@ export APPKEY=$APPKEY
 export TOMAIL=$TOMAIL
 export FROMAIL=$FROMAIL
 export SSHPASSWD=$SSHPASSWD
-
-#Why the hell is this needed if the services don'T use it
-#if [ $SHELL == "/bin/bash" ]; then
-#    
-#elif [ $SHELL == "/usr/bin/zsh" ]; then
-#    sudo echo -e "\nexport GVMUSER=$GVMUSER\nexport GVMPASWD=$GVMPASWD\nexport SSHCON=$SSHCON\nexport APPKEY=\"$APPKEY\"\nexport TOMAIL=$TOMAIL\nexport FROMAIL=$FROMAIL\nexport PORT1=$PORT1\nexport PORT2=$PORT2" >> $HOME/.zshrc
-#else
-#    echo "Unknown Shell"
-#    exit
-#fi
-
-#Creating file containing variables for Services
-sudo echo -e "\nGVMUSER=$GVMUSER\nGVMPASWD=$GVMPASWD\nSSHCON=$SSHCON\nAPPKEY=\"$APPKEY\"\nTOMAIL=$TOMAIL\nFROMAIL=$FROMAIL\nPORT1=$PORT1\nPORT2=$PORT2\nSSHPASSWD=$SSHPASSWD" > /root/scripts/envar.conf
-
 
 #START OF INSTALLATION
 # of openvas for debian according to ("https://greenbone.github.io/docs/latest/22.4/source-build/index.html") with changes for prompted user and password
--- openVas-setup/setup/2.0/sshs.sh
+++ openVas-setup/setup/2.0/sshs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
-spawn ssh -N -o StrictHostKeyChecking=no -R $env(PORT1):localhost:22 $env(SSHCON)
+spawn ssh -N -o StrictHostKeyChecking=no -R $env(PORT1):localhost:22 "$env(SCONU)@$env(SCONI)"
 expect "assword"
 send "$env(SSHPASSWD)\n"
 expect eof

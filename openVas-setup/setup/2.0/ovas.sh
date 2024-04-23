b2abfef1afdd59b64365f422e2f5f1b6b9d276ee
GremlinStyle <152166424+GremlinStyle@users.noreply.github.com>
2024-04-23 17:05:21 +0200

    Wrong expect

--- openVas-setup/setup/2.0/ovas.sh
+++ openVas-setup/setup/2.0/ovas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
 spawn ssh -N -o StrictHostKeyChecking=no -R $env(PORT2):localhost:9392 "$env(SCONU)@$env(SCONI)"
-expect "assword"
+expect "Enter passphrase"
 send "$env(SSHPASSWD)\n"
 expect eof
--- openVas-setup/setup/2.0/sshs.sh
+++ openVas-setup/setup/2.0/sshs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
 spawn ssh -N -o StrictHostKeyChecking=no -R $env(PORT1):localhost:22 "$env(SCONU)@$env(SCONI)"
-expect "assword"
+expect "Enter passphrase"
 send "$env(SSHPASSWD)\n"
 expect eof
